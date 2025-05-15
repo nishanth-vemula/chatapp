cf6bdf9786d57346e437ecc46d7e43bf61ed8aeb
nishanth-vemula <nishanthvemula04@gmail.com>
2025-05-15 16:57:07 +0530

    Add files via upload

--- utils.js
+++ utils.js
@@ -1,13 +1,7 @@
-import jwt from "jsonwebtoken";
-export const generateToken=(userId,res)=>{
-    const token =jwt.sign({userId},process.env.JWT_SECRET,{
-        expiresIn:"7d",
-    })
-    res.cookie("jwt",token,{
-        maxAge:7*24*60*60*1000,
-        httpOnly:true,
-        sameSite:"strict",
-        secure:process.env.NODE_ENV!=="development"
+export function formatMessageTime(date) {
+    return new Date(date).toLocaleTimeString("en-US", {
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: false,
     });
-    return token;
-}
+  }
